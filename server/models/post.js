d8167298ac3b7278bdee0dfd2821859694a960cb
Ron Yu <ronyu77@gmail.com>
2016-12-25 15:20:59 +0800

    feat: Setup model Post

---
+++ server/models/post.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+export default mongoose.model('Post', {
+	text: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	},
+	completed: {
+		type: Boolean,
+		default: false
+	},
+	completedAt: {
+		type: Number,
+		default: null
+	}
+})
